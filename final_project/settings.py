39fcd384c4ee3c2b3d5a653693b8fd151d1d6cdc
sahyiu <167054394+sahyiu@users.noreply.github.com>
2026-01-11 18:51:51 +0800

    isa pa

--- final_project/settings.py
+++ final_project/settings.py
@@ -95,10 +95,8 @@ USE_TZ = True
 
 # Static files configuration
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]                 # Where collectstatic will put files
+STATICFILES_DIRS = [BASE_DIR / "static"]  # or os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR / "staticfiles"    # or os.path.join(BASE_DIR, "staticfiles")
 
 # Media files
 MEDIA_URL = '/media/'
